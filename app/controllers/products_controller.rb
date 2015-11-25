0b4461b69a3c75a92d0c28b9e095add4f10b9863
xiaoyifan <xiaoyifan@uchicago.edu>
2015-11-25 15:34:38 -0600

    added hw3 code for client

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.where(user_id: current_user.uid).all
   end
 
   # GET /products/1
